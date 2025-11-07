00a52373c6fa8d57ed75b9c3f43454d3ce65de64
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-07 11:06:30 +0700

    menambahkan fungsi setData untuk memperbarui state peserta

--- app/src/main/java/com/example/questnavigastugas_222/DataViewModel.kt
+++ app/src/main/java/com/example/questnavigastugas_222/DataViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 data class DataPeserta(
     val nama: String = "",
@@ -17,5 +18,14 @@ class DataViewModel : ViewModel() {
 
     val uiState: StateFlow<DataPeserta> = _uiState.asStateFlow()
 
-
+    fun setData(data: DataPeserta) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                nama = data.nama,
+                alamat = data.alamat,
+                jenisKelamin = data.jenisKelamin,
+                status = data.status
+            )
+        }
+    }
 }
