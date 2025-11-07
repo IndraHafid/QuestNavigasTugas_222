579134fac79b0f3b73ff5daf11442f722d868f53
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-07 11:03:05 +0700

    membuat file DataViewModel.kt dan membuat data class DataPeserta

---
+++ app/src/main/java/com/example/questnavigastugas_222/DataViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.questnavigastugas_222
+
+data class DataPeserta(
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val status: String = ""
+)
+
