978c1c873a7e378ea02251f02b371142a32b17ee
IndraHfid <indrahafidsaputra@gmail.com>
2025-11-07 10:29:21 +0700

    menambahkan judul "Selamat Datang"

--- app/src/main/java/com/example/questnavigastugas_222/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/questnavigastugas_222/view/WelcomeScreen.kt
@@ -1,11 +1,15 @@
 package com.example.questnavigastugas_222.view
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-
+import com.example.questnavigastugas_222.R
 
 @Composable
 fun WelcomeScreen(onNextButtonClicked: () -> Unit) {
@@ -16,6 +20,12 @@ fun WelcomeScreen(onNextButtonClicked: () -> Unit) {
         verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text(
+            text = stringResource(id = R.string.welcome_title),
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold
+        )
+
 
     }
 }
